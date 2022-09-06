7fac0d0179b7f74b8296bc9e3f1aa534ff7c2296
Armando Zapién <azapienc@gmail.com>
2022-09-06 14:35:12 -0500

    feat(NET-5676): Request payroll: error management

--- src/main/java/com/bootcampzapien/assignment_1/publisher/SampleProducer.java
+++ src/main/java/com/bootcampzapien/assignment_1/publisher/SampleProducer.java
@@ -1,6 +1,9 @@
 package com.bootcampzapien.assignment_1.publisher;
 
 import com.bootcampzapien.assignment_1.dto.RequestDto;
+import com.bootcampzapien.assignment_1.mapper.RequestDtoJSONSerializer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -8,6 +11,7 @@ import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -26,6 +30,9 @@ public class SampleProducer {
     private static final Logger log = LoggerFactory.getLogger(SampleProducer.class.getName());
     private final KafkaSender<Integer, String> sender;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     public SampleProducer() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
@@ -40,10 +47,16 @@ public class SampleProducer {
 
     public void sendMessage(String topic, RequestDto requestDto) throws InterruptedException {
         sender.send(Mono.just(requestDto).map(i ->
-                        SenderRecord.create(new ProducerRecord<>(
-                                topic,
-                                requestDto.getEmp_id(),
-                                requestDto.toString()), i))
+                        {
+                            try {
+                                return SenderRecord.create(new ProducerRecord<>(
+                                        topic,
+                                        requestDto.getEmp_id(),
+                                        objectMapper.writeValueAsString(requestDto)), i);
+                            } catch (JsonProcessingException e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
                 )
                 .doOnError(e -> log.error("Send failed", e))
                 .doOnNext(r -> log.info("Message # " + r.correlationMetadata() + " send response: " + r.recordMetadata()))
